5a214f510081e0bf32a43eee3d70a736010f25d7
kamrahul <hi2rahulk@gmail.com>
2022-09-11 18:39:14 +0530

    Added 2 more views
    About page
    contact page

--- src/BasicDjangoApp/BasicDjangoApp/views.py
+++ src/BasicDjangoApp/BasicDjangoApp/views.py
@@ -3,3 +3,11 @@ from django.http import HttpResponse
 
 def home_page(request):
     return HttpResponse('<h1>Hello world</h1>')
+
+
+def about_page(request):
+    return HttpResponse('<h1>About page</h1>')
+
+
+def contact_page(request):
+    return HttpResponse('<h1> Contact page</h1>')
